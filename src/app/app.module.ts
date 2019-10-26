74b7510b33e5478e60e399c54e5633e5cc49e342
Anelvin <amejiaroque@gmail.com>
2019-10-26 02:23:19 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DespensaComponent } from './despensa/despensa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DespensaComponent
   ],
   imports: [
     BrowserModule,
